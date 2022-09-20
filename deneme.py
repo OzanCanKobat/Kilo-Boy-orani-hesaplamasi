a3fcbe52e993b51dd31f89a4b440112e9505804a
Ozan <88509391+OzanCanKobat@users.noreply.github.com>
2022-09-20 16:18:38 +0200

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,15 +1,15 @@
-kilo = int(input("kilonuzu giriniz:"))
-boy = float(input("boyunuzu giriniz:"))
+kilo = int(input("Geben Sie Ihr Gewicht ein:"))
+Größe = float(input("Geben Sie Ihre Körpergröße ein:"))
 
-boykare = (boy ** 2)
+Größequadrat = (Größe ** 2)
 
-islem = (kilo / boykare)
-print(islem)
-if 0 < islem < 18.5:
-    print("zayifsiniz")
-elif 18.5 < islem < 24.9:
+Prozess = (kilo / Großequadrat)
+print(Prozess)
+if 0 < Prozess < 18.5:
+    print("Du bist schwach")
+elif 18.5 < Prozess < 24.9:
     print("normal")
-elif 24.9 < islem < 40.0:
-    print("kilolusunuz")
-elif 40.0 < islem:
-    print("obezsiniz")
+elif 24.9 < Prozess < 40.0:
+    print("du bist übergewichtig")
+elif 40.0 < Prozess:
+    print("Sie sind fettleibig")
